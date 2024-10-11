1e4292c7980822ea220543e62da346e4fe4dc1d9
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-11 20:32:21 -0300

    exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 
 int main() {
+    int numero;
 
-    int opcao = 2;
-    
-    switch (opcao) {
-        case 1:
-            printf("Opção 1 selecionada\n");
-            break;
-        case 2:
-            printf("Opção 2 selecionada\n");
-            break;
-        default:
-            printf("Opção inválida\n");
-            break;
+    // Solicita um número ao usuário
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    // Imprime a tabela de multiplicação
+    printf("Tabela de multiplicação de %d:\n", numero);
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", numero, i, numero * i);
     }
 
     return 0;
