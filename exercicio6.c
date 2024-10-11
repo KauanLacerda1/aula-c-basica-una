544547dee0258aca415a29f35f4d9e04e65da376
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-11 20:34:54 -0300

    exercicio6.c

--- exercicio6.c
+++ exercicio6.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
-    int i = 0;
-    while (i < 5)
-    {
-        printf("i = %d\n", i);
-        i++;
+// Função que calcula o fatorial de um número
+long long fatorial(int n) {
+    if (n <= 1) {
+        return 1; // Fatorial de 0 e 1 é 1
     }
+    return n * fatorial(n - 1); // Chamada recursiva
+}
 
-    return 0;
+int main() {
+    int numero;
+
+    // Solicita um número ao usuário
+    printf("Digite um número inteiro não negativo: ");
+    scanf("%d", &numero);
+
+    // Verifica se o número é não negativo
+    if (numero < 0) {
+        printf("O fatorial não é definido para números negativos.\n");
+    } else {
+        // Chama a função fatorial e exibe o resultado
+        printf("O fatorial de %d é %lld.\n", numero, fatorial(numero));
+    }
+ return 0
 }
