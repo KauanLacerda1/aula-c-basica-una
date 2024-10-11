d97d92548b1f9ed62c73b75beb96b08130c6a42f
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-11 20:31:22 -0300

    exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int numero, i, primo = 1;
 
-    int nota = 85;
+    // Solicita um número ao usuário
+    printf("Digite um número: ");
+    scanf("%d", &numero);
 
-    if (nota >= 90)
-    {
-        printf("Nota A\n");
+    // Verifica se o número é menor que 2
+    if (numero < 2) {
+        primo = 0; // Números menores que 2 não são primos
+    } else {
+        // Verifica se o número é primo
+        for (i = 2; i <= numero / 2; i++) {
+            if (numero % i == 0) {
+                primo = 0; // Encontrou um divisor, não é primo
+                break;
+            }
+        }
     }
-    else if (nota >= 80)
-    {
-        printf("Nota B\n");
-    }
-    else
-    {
-        printf("Nota C\n");
+
+    // Exibe o resultado
+    if (primo) {
+        printf("O número %d é primo.\n", numero);
+    } else {
+        printf("O número %d não é primo.\n", numero);
     }
     
     return 0;
