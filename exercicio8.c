170accca93e9449ff8e6884d979c17c1c4de71f3
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-15 20:50:11 -0300

    Update exercicio8.c

--- exercicio8.c
+++ exercicio8.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 
-int soma(int a, int b) {
-    return a + b;
+void bubbleSort(int arr[], int tamanho) {
+    int i, j, temp;
+
+    // Algoritmo Bubble Sort
+    for (i = 0; i < tamanho - 1; i++) {
+        for (j = 0; j < tamanho - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Troca os elementos se estiverem fora de ordem
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
-int main()
-{
+int main() {
+    int numeros[10];
+    int i;
 
-    int resultado = soma(5, 3);
-    printf("Resultado da soma: %d\n", resultado);
+    // Leitura dos números
+    printf("Digite 10 números inteiros:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Número %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
+    // Ordena o array
+    bubbleSort(numeros, 10);
+
+    // Exibe o array ordenado
+    printf("\nArray em ordem crescente:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
