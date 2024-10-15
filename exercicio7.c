717cce8338ac250432aaa73b7c2c08d4ac3caec1
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-15 20:49:32 -0300

    Update exercicio7.c

--- exercicio7.c
+++ exercicio7.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int numeros[10];
+    int i, maior, menor;
 
-    int j = 0;
-    do
-    {
-        printf("j = %d\n", j);
-        j++;
-    } while (j < 5);
+    // Lê os 10 números inteiros
+    printf("Digite 10 números inteiros:\n");
+    for (i = 0; i < 10; i++) {
+        printf("Número %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
+    // Inicializa o maior e o menor com o primeiro elemento do array
+    maior = menor = numeros[0];
+
+    // Encontra o maior e o menor valor
+    for (i = 1; i < 10; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+        if (numeros[i] < menor) {
+            menor = numeros[i];
+        }
+    }
+
+    // Exibe o resultado
+    printf("\nO maior número é: %d\n", maior);
+    printf("O menor número é: %d\n", menor);
 
     return 0;
 }
