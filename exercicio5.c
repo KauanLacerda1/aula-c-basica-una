d85da7030269883266ef10a502252d2429dd3699
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-15 20:48:21 -0300

    Update exercicio5.c

--- exercicio5.c
+++ exercicio5.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char str[100];
+    int tamanho, i;
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Iteração %d\n", i);
+    printf("Digite uma string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove o caractere de nova linha, se presente
+    tamanho = strlen(str);
+    if (str[tamanho - 1] == '\n') {
+        str[tamanho - 1] = '\0';
+        tamanho--;
     }
+
+    printf("String invertida: ");
+    for (i = tamanho - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
