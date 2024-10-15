c5e0db126351e1d55405426463e8813bfe5eda16
KauanLacerda1 <kauanlsousa2005@gmail.com>
2024-10-15 20:50:52 -0300

    Update exercicio9.c

--- exercicio9.c
+++ exercicio9.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-void saudacao() {
-    printf("Bem-vindo ao programa!\n");
-}
-
 int main() {
-    saudacao();
+    float celsius, fahrenheit;
+
+    // Solicita a temperatura em Celsius
+    printf("Digite a temperatura em Celsius: ");
+    scanf("%f", &celsius);
+
+    // Converte para Fahrenheit
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    // Exibe o resultado
+    printf("%.2f graus Celsius é igual a %.2f graus Fahrenheit.\n", celsius, fahrenheit);
 
     return 0;
 }
